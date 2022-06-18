2197a5801c7edf08cf680709364013cbca7e78ad
d1onni <96235122+d1onni@users.noreply.github.com>
2022-06-18 13:07:20 -0700

    Revert "Category and Store Crud"
    This reverts commit 272d3dc12bfd84e56964595e19c6b968abdd06a1.

--- src/app/common/options/CategoryOptions.ts
+++
@@ -1,4 +0,0 @@
-export const categoryOptions = [
-    {text: 'TestCategory1', value: "TestCategory1"},
-    {text: 'TestCategory2', value: "TestCategory2"},
-]
--- src/app/models/store.ts
+++
@@ -1,5 +0,0 @@
-export interface Store{
-    storeId: string;
-    storeName: string;
-    address: string;
-}
